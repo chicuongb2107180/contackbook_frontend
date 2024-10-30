4fe4bed3d55d95b37fd5c1def10617e8d17d45ca
chicuongb2107180 <cuongb2107180@student.ctu.edu.vn>
2024-10-30 11:34:09 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,43 @@
+<template>
+        <div class="page">
+                <h4>Thêm liên hệ</h4>
+                <ContactForm  @submit:contact="createContact" :contact="contact" />
+                <p>{{ message }}</p>        
+        </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/Contact_service.js";
+export default{
+        components:{
+                ContactForm
+        },
+        data(){
+                return{
+                        contact:null,
+                        message:""
+                };
+        },
+        methods:{
+                async createContact(data){
+                        try{    
+                                await ContactService.create(data);
+                                this.message="Liên hệ đã được thêm thành công";
+                                this.$router.push({name:"contactbook"});
+                        }catch(error){
+                                console.log(error);
+                        }
+                }
+        },
+        created(){
+                this.contact={
+                        name:"",
+                        email:"",
+                        address:"",
+                        phone:"",
+                        favorite:false,
+                };
+                this.message="";
+        }
+};
+</script>
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -118,7 +118,7 @@ export default {
       }
     },
     async gotoAddContact() {
-      this.$router.push({ name: "AddContact" });
+      this.$router.push({ name: "contact.add" });
     },
   },
   mounted() {
