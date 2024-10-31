d1d7548889e196a0dac33df5e5e704e187f2d036
chicuongb2107180 <cuongb2107180@student.ctu.edu.vn>
2024-10-31 17:51:00 +0700

    fix bug

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -7,7 +7,7 @@
 </template>
 <script>
 import ContactForm from "@/components/ContactForm.vue";
-import ContactService from "@/services/Contact_service.js";
+import ContactService from "@/services/contact_service.js";
 export default{
         components:{
                 ContactForm
